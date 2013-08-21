601b874c89d0b54b1fe23afc10434829a04c049a
Ryan Brush <rbrush@cerner.com>
2013-08-20 20:55:09 -0500

    Add storm eventing example.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clara/clara-rules "0.1.0-SNAPSHOT"]
+                 [clara/clara-storm "0.1.0-SNAPSHOT"]
                  [clj-time "0.5.1"]]
   :main clara.examples
   :aot :all)
