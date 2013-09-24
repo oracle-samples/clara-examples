5fcdf1e507924f567b3c13a6bb9af232115319d4
Ryan Brush <rbrush@cerner.com>
2013-09-23 20:54:16 -0500

    Use release version of clara-rules.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clara/clara-examples "0.1.0-SNAPSHOT"
+(defproject org.toomuchcode/clara-examples "0.1.0-SNAPSHOT"
   :description "Clara Example Rules"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/rbrush/clara-examples"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clara/clara-rules "0.1.0-SNAPSHOT"]
+                 [org.toomuchcode/clara-rules "0.1.0"]
                  [clara/clara-storm "0.1.0-SNAPSHOT"]
                  [clj-time "0.5.1"]]
   :source-paths ["src/main/clojure"]
