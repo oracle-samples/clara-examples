37691c9e1844d1dbc809cff1cd51668f397339ed
Ryan Brush <rbrush@gmail.com>
2015-01-26 23:30:57 -0600

    Upgrade Clojure, ClojureScript, Clara versions.

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.google.guava/guava "15.0"] ; Explicitly pull new Guava version for dependency conflicts.
-                 [org.clojure/clojure "1.5.1"]
-                 [org.toomuchcode/clara-rules "0.7.0"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.toomuchcode/clara-rules "0.9.0-SNAPSHOT"]
 
                  ;; Dependencies for ClojureScript example.
-                 [prismatic/dommy "0.1.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [prismatic/dommy "0.1.3"]
+                 [org.clojure/clojurescript "0.0-2727"]
 
                  ;; Dependency for time-based rules example.
                  [clj-time "0.6.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :plugins [[lein-cljsbuild "1.0.4"]]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :java-source-paths ["src/main/java"]
@@ -26,7 +26,7 @@
                                    :optimizations :advanced}}]}
 
   ;; Austin for the ClojureScript REPL.
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.3"]]}}
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.6"]]}}
 
   :scm {:name "git"
         :url "https://github.com/rbrush/clara-examples.git"}
