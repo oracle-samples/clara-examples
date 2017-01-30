5cb6712e79209c4f371524edc96db54e35e15910
William Parker <wparker30@yahoo.com>
2017-01-30 12:00:47 -0600

    Remove example using the durability API that has been removed.  This will be replaced later with a new example using the new API.

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[com.google.guava/guava "15.0"] ; Explicitly pull new Guava version for dependency conflicts.
                  [org.clojure/clojure "1.7.0"]
-                 [org.toomuchcode/clara-rules "0.11.0"]
-                 [com.cerner/clara-rules "0.13.0-RC4"]
+                 [com.cerner/clara-rules "0.13.0-RC7"]
                  [org.toomuchcode/clara-tools "0.1.1"]
 
                  ;; Dependencies for ClojureScript example.
